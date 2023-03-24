dce3f9aed8b3c54582f247495f953380d29c4650
Sylvan22 <sylvan.arnold@hes-so.ch>
2023-03-24 15:37:31 +0100

    CAN read simple function

--- main.c
+++ main.c
@@ -1,46 +1,103 @@
-/************************************************************************//**
- * \file	main.c
- * \brief	Program main file for MODBUS controller
- ***************************************************************************/
-
-
 /**
- * Include list
- */
-#include <xc.h>
+  Generated Main Source File
 
-#include <stdint.h>
-#include <stdbool.h>
-#include <stdio.h>
+  Company:
+    Microchip Technology Inc.
 
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.8
+        Device            :  PIC18F97J60
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
 #include "can.h"
-#include "lcd/lcd.h"
-//To Be Completed
 
-
-/**
- * Initialize clock, buttons, leds and interrupts
+/*
+                         Main application
  */
-void init_hw() {
+void main(void)
+{
+    // Initialize the device
+    SYSTEM_Initialize();
+
+    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
+    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
     
-    Lcd_Init(LCD_2x16);
-    //To Be Completed
+    CanInit(1, CAN_250K_1M);
     
-}
-#define _XTAL_FREQ  64000000L
+    uint8_t idCar = 0x8;
+    CS_SetHigh();
+    
 
-void main(void) {
-	OSCCON3 = 0;            // for 64MHz cpu clock (default is 32MHz)
-    init_hw();              // Init hardware
-                            //Call the init_hw function that sets up the start of our Picebs with base informations. Like general interrupts
-    LCD_2x16_WriteMsg("Welcome to ...  ",0);      //Display on line 0
-    LCD_2x16_WriteMsg("CAN-CAR demo !  ",1);      //Display on line 1
+        CAN_TX_MSGOBJ foo;
+        CAN_RX_MSGOBJ rxObj;
+        uint8_t txd[1];
+        uint8_t rxd[1];
+        
 
-    //To Be Completed
-
-    //--------------------------------------------------------------------------
-    // Infinite loop
-       while(true){
-		   //To Be Completed
+        
+    while (1) {
+        if(CanReceive(&rxObj,rxd) == 0) {
+          if(rxObj.bF.id.ID = (0x7 << 4) | idCar) {
+            txd[0] = rxd[0];
+            foo.bF.id.ID = (0x11 << 4) | idCar;
+            foo.bF.ctrl.DLC = CAN_DLC_1; // number of byte to send
+            foo.bF.ctrl.RTR = 0;
+            foo.bF.id.SID11 = 0;
+            foo.bF.ctrl.FDF = 0;
+            foo.bF.ctrl.IDE = 0;
+            foo.bF.ctrl.BRS = 0;
+            foo.bF.ctrl.ESI = 0;
+            CanSend(&foo, txd);
+          }
+        }
     }
 }
+/**
+ End of File
+*/
--- spi.c
+++ spi.c
@@ -22,116 +22,27 @@
 
 #include <stdint.h>
 
+#include "mcc_generated_files/pin_manager.h"
+
 volatile uint8_t * ptrCS_SPI;
 uint8_t csBitNr;
 
 uint8_t  mikroNr=0;
-int8_t SpiInit(uint8_t mikroBusNr)
-{
-  if((mikroBusNr > 5) | (mikroBusNr < 1))
-    return -1;
-  mikroNr = mikroBusNr;
-  // ---------------------------------------------------------------------------
-  // inits pins for communication
-  // ---------------------------------------------------------------------------
-  switch(mikroBusNr)
-  {
-    case 1:
-      RPOR26_27bits.RPO26R = 5;   // assign SKC2 to pin R26 (RD6)
-      RPOR24_25bits.RPO24R = 4;   // assign MOSI2 to pin R24 (RD4)
-      RPINR12_13bits.SDI2R = 6;   // assign MISO2 to pin R25 (RD5)
-      ptrCS_SPI = &LATL;             // assign chip select
-      csBitNr = 1;
-      TRISL1 = 0;                 // set it as output
-      TRISDbits.TRISD6 = 0;       // SKC2 is an output
-      TRISDbits.TRISD4 = 0;       // MOSI2 is an output
-      break;
-    case 2:
-      RPOR26_27bits.RPO26R = 5;   // assign SKC2 to pin R26 (RD6)
-      RPOR24_25bits.RPO24R = 4;   // assign MOSI2 to pin R24 (RD4)
-      RPINR12_13bits.SDI2R = 6;   // assign MISO2 to pin R25 (RD5)
-      ptrCS_SPI = &LATL;             // assign chip select
-      csBitNr = 2;
-      TRISL2 = 0;                 // set it as output
-      TRISDbits.TRISD6 = 0;       // SKC2 is an output
-      TRISDbits.TRISD4 = 0;       // MOSI2 is an output
-      break;
-    case 3:
-      RPOR26_27bits.RPO26R = 5;   // assign SKC2 to pin R26 (RD6)
-      RPOR24_25bits.RPO24R = 4;   // assign MOSI2 to pin R24 (RD4)
-      RPINR12_13bits.SDI2R = 6;   // assign MISO2 to pin R25 (RD5)
-      ptrCS_SPI = &LATL;             // assign chip select
-      csBitNr = 3;
-      TRISL3 = 0;                 // set it as output
-      TRISDbits.TRISD6 = 0;       // SKC2 is an output
-      TRISDbits.TRISD4 = 0;       // MOSI2 is an output
-      break;
-    case 4:
-      RPOR22_23bits.RPO23R = 3;   // assign SKC1 to pin R23 (RD3)
-      RPOR20_21bits.RPO21R = 4;   // assign MOSI1 to pin R21 (RD1)
-      RPINR8_9bits.SDI1R = 5;     // assign MISO1 to pin 20 (RD0)
-      ptrCS_SPI = &LATL;             // assign chip select
-      csBitNr = 4;
-      TRISL4 = 0;                 // set it as output
-      TRISDbits.TRISD3 = 0;       // SKC1 is an output
-      TRISDbits.TRISD1 = 0;       // MOSI1 is an output
-      break;
-    case 5:
-      RPOR22_23bits.RPO23R = 3;   // assign SKC1 to pin R23 (RD3)
-      RPOR20_21bits.RPO21R = 4;   // assign MOSI1 to pin R21 (RD1)
-      RPINR8_9bits.SDI1R = 5;     // assign MISO1 to pin 20 (RD0)
-      ptrCS_SPI = &LATL;             // assign chip select
-      csBitNr = 5;
-      TRISL5 = 0;                 // set it as output
-      TRISDbits.TRISD3 = 0;       // SKC1 is an output
-      TRISDbits.TRISD1 = 0;       // MOSI1 is an output
-      break;
-  }
-  // ---------------------------------------------------------------------------
-  // inits MSSP for communication
-  // ---------------------------------------------------------------------------
-  if(mikroBusNr < 4)              // MSSP2 is used in this case
-  {
-    SSP2STATbits.CKE = 1;         // clock phase
-    SSP2CON1bits.CKP = 0;         // clock is active high
-    SSP2CON1bits.SSPEN = 1;       // enable serial port
-    SSP2CON1bits.SSPM = 0b0000;   // SPI clock is 4MHz (Fosc/16)
-  }
-  else                            // MSSP1 (caution with touch)
-  {
-    SSP1STATbits.CKE = 1;         // clock phase
-    SSP1CON1bits.CKP = 0;         // clock is active high
-    SSP1CON1bits.SSPEN = 1;       // enable serial port
-    SSP1CON1bits.SSPM = 0b0000;   // SPI clock is 4MHz (Fosc/16)
-  }
-  return 0;
-}
 
-int8_t SpiTransfer(uint8_t * txPtr, uint8_t * rxPtr, uint16_t size)
-{
+int8_t SpiTransfer(uint8_t * txPtr, uint8_t * rxPtr, uint16_t size) {
   volatile uint8_t dummy;
-  *ptrCS_SPI &= ~(1 << csBitNr);                     // activate chip select
-  while(size > 0)
-  {
-    if(mikroNr < 4)               // MSSP2 is used
-    {
-      //SSP2STATbits.BF = 0;        // clear transfer flag
-      dummy = SSP2BUF;
-      SSP2BUF = *txPtr;           // send data
-      while(SSP2STATbits.BF == 0){}
-      *rxPtr = SSP2BUF;           // get received data
-    }
-    else                          // MSSP1 is used
-    {
-      SSP2STATbits.BF = 0;         // clear transfer flag
-      SSP1BUF = *txPtr;           // send data
-      while(SSP1STATbits.BF == 0){}
-      *rxPtr = SSP1BUF;           // get received data
-    }
+  CS_SetLow();
+  while(size > 0) {
+    //SSP2STATbits.BF = 0;        // clear transfer flag
+    dummy = SSP2BUF;
+    SSP2BUF = *txPtr;           // send data
+    while(SSP2STATbits.BF == 0){}
+    *rxPtr = SSP2BUF;           // get received data
+    
     txPtr++;                      // increment tx pointer
     rxPtr++;                      // increment rx pointer
     size--;                       // decrement bytes counter
   };
-  *ptrCS_SPI |= (1 << csBitNr);                     // deactivate chip select
+  CS_SetHigh();                    // deactivate chip select
   return 0;
 }
